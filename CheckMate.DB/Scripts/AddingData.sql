f702bba7a28bed0ca4aa54fe0cebf0a9a6695d67
Nicolas Gihoul <ngihoul@hotmail.com>
2024-12-04 08:31:19 +0100

    Creating Tables and PostScripots for User and Role

---
+++ CheckMate.DB/Scripts/AddingData.sql
@@ -0,0 +1,19 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+SET IDENTITY_INSERT [Role] ON
+
+INSERT INTO [Role] ([Id], [Name]) VALUES
+            (1, 'User'),
+            (2, 'Admin')
+
+SET IDENTITY_INSERT [Role] OFF
