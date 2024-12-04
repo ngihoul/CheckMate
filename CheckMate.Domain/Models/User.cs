fb6cc2b939329ec42e907f680908d0f40c2c4163
Nicolas Gihoul <ngihoul@hotmail.com>
2024-12-04 11:39:55 +0100

    Changing nullable values

--- CheckMate.Domain/Models/User.cs
+++ CheckMate.Domain/Models/User.cs
@@ -10,10 +10,10 @@ namespace CheckMate.Domain.Models
     public class User
     {
         public int Id { get; set; }
-        public string Username { get; set; }
+        public string? Username { get; set; }
         public string Email { get; set; }
-        public string Password { get; set; }
-        public string Salt { get; set; }
+        public string? Password { get; set; }
+        public string? Salt { get; set; }
         public DateTime DateOfBirth { get; set; }
         public char Gender { get; set; }
         public int Elo { get; set; } = 1200;
