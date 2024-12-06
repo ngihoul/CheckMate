35ab1d9c90eaa61b5f649a77b707cf5ef46f26c2
Nicolas Gihoul <ngihoul@hotmail.com>
2024-12-06 16:26:23 +0100

    Create GameRepository

--- CheckMate.DAL/Repositories/GameRepository.cs
+++ CheckMate.DAL/Repositories/GameRepository.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CheckMate.DAL.Interfaces;
+using CheckMate.Domain.Models;
+using Microsoft.Data.SqlClient;
 
 namespace CheckMate.DAL.Repositories
 {
-    public class GameRepository
+    public class GameRepository : IGameRepository
     {
+        private readonly SqlConnection _connection;
+
+        public GameRepository(SqlConnection connection)
+        {
+            _connection = connection;
+        }
+
+        public Task<Game> Create(Game game)
+        {
+            SqlCommand sqlCommand = _connection.CreateCommand();
+
+            sqlCommand.CommandText = "INSERT INTO [Game] ([TournamentId], [WhiteId], [BlackId], [Round], [Winner]) " +
+                                    "OUTPUT Inserted.Id " +
+                                    // To check if 1 for Winner is ok
+                                    "VALUES (@TournamentId, @WhiteId, @BlackId, @Round, 1);";
+        }
     }
 }
